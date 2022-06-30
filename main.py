93e0cb36804bc12a364d5388f0d666ee1d822c6b
Emily Berger <eberge11@jhu.edu>
2022-06-30 17:46:33 +0000

    temp commit will work on later

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import requests
+import pandas as pd
 
 TECHY_API_URL = "https://techy-api.vercel.app/api/json"
 
-techy_response = requests.get(TECHY_API_URL).json()
+responses = {}
 
-print(techy_response['message'])
+for i in range(20):
+    techy_response = requests.get(TECHY_API_URL).json()['message']
+    responses['i'] = techy_response
+
+data = pd.DataFrame.from_dict(responses, orient="index", columns=["num", "data"])
+print(data)
+
+
+#lets create a chat bot that 
